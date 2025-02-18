ea2472e725e5c36664535d124969a810b9ac3c85
So8oro <voo870@gmail.com>
2025-02-18 17:55:32 +0900

    update

---
+++ 20250217_Stack2_1/4874. Forth (제출용).py
@@ -0,0 +1,56 @@
+T = int(input())
+
+for tc in range(1,T+1):
+
+    arr = list(input().split())
+
+    stack = [0]*len(arr)
+    top = -1
+
+    for i in arr:
+        try:
+            if i not in ('+','-','*','/','.'):
+                top += 1
+                stack[top] = int(i)
+            elif i=='+':
+                stack[top-1] = stack[top]+stack[top-1]
+                top -= 1
+            elif i=='-':
+                stack[top-1] = stack[top-1]-stack[top]
+                top -= 1
+            elif i=='*':
+                stack[top-1] = stack[top]*stack[top-1]
+                top -= 1
+            elif i=='/':
+                stack[top-1] = stack[top-1]//stack[top]
+                top -= 1
+            else:
+                if top == 0:
+                    print(f'#{tc} {stack[0]}')
+                else:
+                    print(f'#{tc} error')
+
+        except:
+            print(f'#{tc} error')
+
+
+
+
+    # infix = input()
+    # postfix = ''
+    # stack = [0] * 3
+    # top = -1
+    # icp = {'+': 1, '*': 2}  # 연산자 우선순위
+    # for i in range(3):
+    #     if '0' <= infix[i] <= '9':  # 피연산자인 경우
+    #         postfix += infix[i]
+    #     else:  # 연산자인 경우
+    #         if top > -1 and icp[stack[top]] >= icp[infix[i]]:  # stack[top] 우선순위가 같거나 높으면  pop
+    #             postfix += stack[top]
+    #             top -= 1
+    #         top += 1
+    #         stack[top] = infix[i]
+    # while top > -1:  # 남아있는 연산자를 수식뒤에 붙임
+    #     postfix += stack[top]
+    #     top -= 1
+    # print(postfix)
