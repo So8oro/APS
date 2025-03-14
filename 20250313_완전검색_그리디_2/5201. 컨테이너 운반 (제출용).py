be892b3fa858364def9056a60f7d426282e59798
So8oro <voo870@gmail.com>
2025-03-14 11:22:47 +0900

    update

--- 20250313_완전검색_그리디_2/5201. 컨테이너 운반 (제출용).py
+++ 20250313_완전검색_그리디_2/5201. 컨테이너 운반 (제출용).py
@@ -1,3 +1,26 @@
 import sys
 sys.stdin = open('input.txt','r')
 
+T = int(input())
+
+for tc in range(1,T+1):
+
+    N,M = map(int,input().split())
+    Wi = list(map(int,input().split()))
+    Ti = list(map(int,input().split()))
+
+    Wi.sort(reverse=1)
+    Ti.sort(reverse=1)
+
+    weight_sum = 0
+
+    for truck in Ti:
+        while Wi:
+            if truck >= Wi[0]:
+                weight_sum += Wi[0]
+                Wi.pop(0)
+                break
+            else:
+                Wi.pop(0)
+
+    print(f'#{tc} {weight_sum}')
